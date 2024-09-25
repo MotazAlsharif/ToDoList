0f74158a44e77527e0884b3e0c20d70562aadb2e
MotazAlsharif <motazalshari3@gmail.com>
2024-09-25 21:11:14 +0300

    Add project files.

---
+++ ToDoList/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using ToDoList.ViewModel;
+
+namespace ToDoList
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage(MainViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext= (vm);
+        }
+
+     
+    }
+
+}
---
+++ ToDoList/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+
+
+namespace ToDoList.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel() { 
+            items=new ObservableCollection<string>();
+        }
+
+       [ObservableProperty]
+       ObservableCollection<string> items;
+        
+       [ObservableProperty]
+       private string text;
+
+
+       [RelayCommand]
+       private void Add()
+       {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+            
+            items.Add(Text);
+          Text = string.Empty;
+       }
+        [RelayCommand]
+        void Delete(string d)
+        {
+            if (items.Contains(d)) 
+            {
+                Items.Remove(d);
+            }
+        }
+    }
+}
